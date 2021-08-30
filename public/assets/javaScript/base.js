0185e70bab8e271b8bf4a0a566afffb80c80ff0b
Julien-Perrotgit config --global user.email p.julien.perrot@hotmail.com <p.julien.perrot@hotmail.com>
2021-08-30 14:16:13 +0200

    Front, base.html.twig + picture CSS & JS

---
+++ public/assets/javaScript/base.js
@@ -0,0 +1,21 @@
+// Fonction pour le scroll de ma nav_bar :
+$(document).ready(function(){
+	$(window).bind('scroll', function() {
+		let navHeight = $('.header').height();
+		if ($(window).scrollTop() > navHeight) {
+			$('.navbar').addClass('fixed');
+		 }
+		else {
+			$('.navbar').removeClass('fixed');
+		 }
+	});
+});
+
+//Fonctions phrases footer
+let texts = ["Site breveté et créer ...", "par une équipe ...", "formée par Webforce 3.", "Lavdon,", "Nicolas,", "Ghislain,", "Julien."];
+let count = 0;
+    function changeText() {
+        $(".typewrite").text(texts[count]);
+        count < 7 ? count++ : count = 0;
+    }
+setInterval(changeText, 2000);
