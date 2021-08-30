681066dcaba9a56e225095836bf0c489a303e445
Nico <85336816+NicoCode-alt@users.noreply.github.com>
2021-08-30 11:52:47 +0200

    Ajoute view/add/delete Commentaires

---
+++ migrations/Version20210830092629.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210830092629 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE commentary (id INT AUTO_INCREMENT NOT NULL, critic_id INT NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_1CAC12CAC7BE2830 (critic_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE commentary ADD CONSTRAINT FK_1CAC12CAC7BE2830 FOREIGN KEY (critic_id) REFERENCES critic (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE commentary');
+    }
+}
